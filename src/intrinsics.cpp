d660c8cfefb91cd1743360406db67488edddd1f7
Zentrik <Zentrik@users.noreply.github.com>
2023-10-20 18:32:00 +0100

    Actually insert alloca in dynamic case

--- src/intrinsics.cpp
+++ src/intrinsics.cpp
@@ -1319,8 +1319,8 @@ static jl_cgval_t emit_intrinsic(jl_codectx_t &ctx, intrinsic f, jl_value_t **ar
             Type *length_type = INTT(bitstype_to_llvm(jl_length.typ, ctx.builder.getContext(), true), DL);
             Value *length = emit_unbox(ctx, length_type, jl_length, jl_length.typ);
 
-            // taken from emit_static_alloca
-            loc = new AllocaInst(eltype, ctx.topalloca->getModule()->getDataLayout().getAllocaAddrSpace(), length, Align(jl_datatype_align(jl_eltype)), "");
+            // taken from emit_static_alloca and CreateAlloca
+            loc = ctx.builder.Insert(new AllocaInst(eltype, ctx.topalloca->getModule()->getDataLayout().getAllocaAddrSpace(), length, Align(jl_datatype_align(jl_eltype))), "");
         }
 
         jl_value_t *rt = (jl_value_t*)jl_apply_type1((jl_value_t*)jl_pointer_type, jl_eltype);
