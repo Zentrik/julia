c0305f5995c1e1094711199ee55c18670b0af189
Zentrik <llvm.zentrik@gmail.com>
2025-04-16 20:17:17 +0100

    c04d9d57ee4c116ecc1222b1f4458a21be13ac15
    Adapt to "MCAsmStreamer: Replace the MCInstPrinter * parameter with unique_ptr #135128 "

--- src/disasm.cpp
+++ src/disasm.cpp
@@ -927,7 +927,7 @@ static void jl_dump_asm_internal(
 #if JL_LLVM_VERSION >= 190000
         TheTarget->createAsmStreamer(Ctx, std::move(ustream),
 
-                                     IP.release(), std::move(CE), std::move(MAB))
+                                     std::move(IP), std::move(CE), std::move(MAB))
 #else
         TheTarget->createAsmStreamer(Ctx, std::move(ustream), /*asmverbose*/ true,
                                      /*useDwarfDirectory*/ true, IP.release(),
@@ -1268,8 +1268,8 @@ jl_value_t *jl_dump_function_asm_impl(jl_llvmf_dump_t* dump, char emit_mc, const
                 OutputAsmDialect = 0;
             if (!strcmp(asm_variant, "intel"))
                 OutputAsmDialect = 1;
-            MCInstPrinter *InstPrinter = TM->getTarget().createMCInstPrinter(
-                jl_ExecutionEngine->getTargetTriple(), OutputAsmDialect, MAI, MII, MRI);
+            std::unique_ptr<MCInstPrinter> InstPrinter(TM->getTarget().createMCInstPrinter(
+                jl_ExecutionEngine->getTargetTriple(), OutputAsmDialect, MAI, MII, MRI));
             std::unique_ptr<MCAsmBackend> MAB(TM->getTarget().createMCAsmBackend(
                 STI, MRI, Options));
             std::unique_ptr<MCCodeEmitter> MCE;
@@ -1279,7 +1279,7 @@ jl_value_t *jl_dump_function_asm_impl(jl_llvmf_dump_t* dump, char emit_mc, const
             auto FOut = std::make_unique<formatted_raw_ostream>(asmfile);
             std::unique_ptr<MCStreamer> S(TM->getTarget().createAsmStreamer(
 #if JL_LLVM_VERSION >= 190000
-                *Context, std::move(FOut), InstPrinter, std::move(MCE), std::move(MAB)
+                *Context, std::move(FOut), std::move(InstPrinter), std::move(MCE), std::move(MAB)
 #else
                 *Context, std::move(FOut), true, true, InstPrinter, std::move(MCE),
                 std::move(MAB), false
